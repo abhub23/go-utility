b2f4804a27dc791f019f0a7ae0e6fd2212ec2e95
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-02-13 23:59:16 +0530

    modified

--- rim.go
+++ rim.go
@@ -4,7 +4,7 @@ import (
 	"strings"
 )
 
-func Calc(name1 string, name2 string) string {
+func Flames(name1 string, name2 string) string {
 	lower1 := strings.ToLower(name1)
 	lower2 := strings.ToLower(name2)
 	return lower1 + " Flames " + lower2
