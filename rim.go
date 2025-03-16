a0d71d5b227a3e1efd0e1d25e5a7298efd1ee7da
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-03-16 20:18:13 +0530

    refactor

--- rim.go
+++ rim.go
@@ -5,7 +5,25 @@ import (
 )
 
 func Flames(name1 string, name2 string) string {
-	lower1 := strings.ToLower(name1)
-	lower2 := strings.ToLower(name2)
-	return lower1 + " Flames " + lower2
+	name1 = strings.ToLower(strings.ReplaceAll(name1, " ",""))
+	name2 = strings.ToLower(strings.ReplaceAll(name2, " ",""))
+	Slice := []byte{}
+	for i := 0; i< len(name1); i++ {
+		for j := 1; j < len(name2); j++{
+			if name1[i] != name2[j] {
+				Slice = append(Slice, name1[i])
+			}
+		}
+	} 
+
+	flamesmap := make(map[rune]string)
+
+	flamesmap['F'] = "Friends"
+	flamesmap['L'] = "Lovers"
+	flamesmap['A'] = "Affection"
+	flamesmap['M'] = "Marriage"
+	flamesmap['E'] = "Enemies"
+	flamesmap['S'] = "Siblings"
+
+	return string(Slice)
 }
