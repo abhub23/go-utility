be1bd2a44453ccbc453d140fffda31d2c6e81f93
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-06-10 21:55:16 +0530

    test cases added in structs

--- test/rim_test.go
+++ test/rim_test.go
@@ -6,14 +6,29 @@ import (
 )
 
 func TestFlames(t *testing.T) {
-	got, err := rim.Flames("Javascript", "Typescript")
-	if err != nil {
-		t.Errorf("Error occured %v", err)
+
+	type Tests struct {
+		testName     string
+		name1, name2 string
+		Expected     string
 	}
 
-	want := "javascript and typescript have a relation of Affection"
-
-	if got != want {
-		t.Errorf("Flames(Abdullah, Rimsha) got %s and want %s", got, want)
+	tests := []Tests{
+		{"Javascript and Typescript", "Javascript", "Typescript", "javascript and typescript have a relation of Affection"},
+		{"Go and Rust", "Go", "Rust", "go and rust have a relation of Marriage"},
 	}
+
+	for _, el := range tests {
+		t.Run(el.testName, func(t *testing.T) {
+			result, err := rim.Flames(el.name1, el.name2)
+			if err != nil {
+				panic(err)
+			}
+
+			if result != el.Expected{
+				t.Errorf("func was expecting %s but got %s ", el.Expected, result )
+			}
+		})
+	}
+
 }
